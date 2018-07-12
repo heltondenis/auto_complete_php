36d7c1d031a7514b4ad7c7090d75b45ce79dd6c8
Helton Denis <heltondeniss@gmail.com>
2018-07-11 21:47:03 -0300

    função ajax

--- index.php
+++ index.php
@@ -9,20 +9,21 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="estilo.css">
 
-
-    <title>Sistema de autocompletar</title>
+    <title>Autocomplete!</title>
   </head>
   <body>
-
-
 <form id="content">
-  <input type="text" name="input" class="input">
+  <input type="text" name="busca" id="busca" class="input">
   <button type="reset" class="search"></button>
+
+  <div id="resultado"></div>
 </form>
 
+    <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="script.js" type="text/javascript" charset="utf-8" async defer></script>
   </body>
 </html>
--- script.js
+++ script.js
@@ -9,3 +9,19 @@ function expand() {
   }
 }
 $('button').on('click', expand);
+
+$(function(){
+	$('#busca').on('keyup', function() {
+
+		var texto = $(this).val();
+
+		$.ajax({
+           url:'busca.php',
+           type: 'POST',
+           data: {texto:texto},
+           success:function(html){
+           	$('#resultado').html(html);
+           }
+		});
+	});
+})
